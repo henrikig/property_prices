c6be1e6cfc13d8b4e5f722bfa2d6e746abee453d
Henrik Irgens Gravdal <henrikigravdal@gmail.com>
2020-03-24 15:03:59 +0100

    Exception added for properties without price or size

--- properties/spiders/properties_spider.py
+++ properties/spiders/properties_spider.py
@@ -22,13 +22,17 @@ class PropertiesSpider(scrapy.Spider):
                 # Parse price, split on hyphen
                 item['price'] = (re.sub('[^0-9,-]', "", keys[1])).split("-")
 
-                item['size'][0] = int(item['size'][0])
-                item['price'][0] = int(item['price'][0])
+                try:
+                    item['size'][0] = int(item['size'][0])
+                    item['price'][0] = int(item['price'][0])
+
+                except ValueError:
+                    continue
 
                 if len(item['size']) > 1:
                     item['size'][1] = int(item['size'][1])
                 if len(item['price']) > 1:
-                    item['price'][1] = int(item['price'][1])
+                    item['price'][1] = int(item['price'][1]) if item['price'][1] != '' else 0
 
                 item['location'] = location
 
